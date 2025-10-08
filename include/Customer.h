3b819b820de27cdc06042a4f35fb999be054336d
mariam <amer.mariam02@outlook.com>
2025-10-08 14:41:10 +0300

    Update Customer.h

--- include/Customer.h
+++ include/Customer.h
@@ -1,51 +1,62 @@
+#ifndef CUSTOMER_H_
+#define CUSTOMER_H_
+
 #pragma once
 #include <string>
 #include <vector>
-using std::string;
-using std::vector;
 
-
+/* ============================
+          Customer
+============================ */
 
 class Customer {
-    public:
-        Customer(int id, const string &name, int locationDistance, int maxOrders);
-        const string &getName() const;
-        int getId() const;
-        int getCustomerDistance() const;
-        int getMaxOrders() const; //Returns maxOrders
-        int getNumOrders() const; //Returns num of orders the customer has made so far
-        bool canMakeOrder() const; //Returns true if the customer didn't reach max orders
-        const vector<int> &getOrdersIds() const;
-        int addOrder(int orderId); //return OrderId if order was added successfully, -1 otherwise
-        
-        virtual Customer *clone() const = 0; // Return a copy of the customer
-        virtual ~Customer()=default;
-        
-    private:
-        const int id;
-        const string name;
-        const int locationDistance;
-        const int maxOrders;
-        vector<int> ordersId;
+public:
+    Customer(int id, const std::string& name, int locationDistance, int maxOrders);
+
+    // Getters
+    const std::string& getName() const;
+    int getId() const;
+    int getCustomerDistance() const;
+    int getMaxOrders() const;
+    int getNumOrders() const;
+    bool canMakeOrder() const;
+    const std::vector<int>& getOrdersIds() const;
+
+    // Order management
+    int addOrder(int orderId); // Returns orderId if added successfully, -1 otherwise
+
+    // Polymorphic interface
+    virtual Customer* clone() const = 0;
+    virtual ~Customer() = default;
+
+private:
+    const int id;
+    const std::string name;
+    const int locationDistance;
+    const int maxOrders;
+    std::vector<int> ordersId;
 };
 
+/* ============================
+       SoldierCustomer
+============================ */
 
-class SoldierCustomer: public Customer {
-    public:
-        SoldierCustomer(int id, const string &name, int locationDistance, int maxOrders);
-        SoldierCustomer *clone() const override;
-        virtual ~SoldierCustomer()=default;
-    
-    private:
-        
+class SoldierCustomer : public Customer {
+public:
+    SoldierCustomer(int id, const std::string& name, int locationDistance, int maxOrders);
+    SoldierCustomer* clone() const override;
+    ~SoldierCustomer() override = default;
 };
 
-class CivilianCustomer: public Customer {
-    public:
-        CivilianCustomer(int id, const string &name, int locationDistance, int maxOrders);
-        CivilianCustomer *clone() const override;
-        virtual ~CivilianCustomer()=default;
-    
-    private:
-        
+/* ============================
+       CivilianCustomer
+============================ */
+
+class CivilianCustomer : public Customer {
+public:
+    CivilianCustomer(int id, const std::string& name, int locationDistance, int maxOrders);
+    CivilianCustomer* clone() const override;
+    ~CivilianCustomer() override = default;
 };
+
+#endif // CUSTOMER_H_
